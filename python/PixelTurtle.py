1f24382602ab61ef8ae09df6f4e02d3852d58c38
Murilo Polese <murilopolese@gmail.com>
2018-10-01 22:03:16 +0100

    Add colors on PixelTurtle

--- python/PixelTurtle.py
+++ python/PixelTurtle.py
@@ -1,5 +1,14 @@
 import PixelKit as kit
 
+red = [10, 0, 0]
+green = [0, 10, 0]
+blue = [0, 0, 10]
+yellow = [10, 10, 0]
+cyan = [0, 10, 10]
+purple = [10, 0, 10]
+black = [0, 0, 0]
+white = [10, 10, 10]
+
 # Current position of Pixel [x, y]
 cursor = [8, 4]
 # Current color of Pixel
