274ac9bb25c9dcef30b4fe941e152dbe3445466f
Murilo Polese <murilopolese@gmail.com>
2018-10-04 10:58:58 +0100

    Split build scripts from source files

--- python/flash.sh
+++ python/flash.sh
@@ -6,22 +6,22 @@ esptool.py -p $1 write_flash 0x1000 ./esp32-20181002-v1.9.4-623-g34af10d2e.bin >
 sleep 1
 
 echo 'Removing default boot.py'
-ampy -p $1 rm boot.py
+ampy -p $1 rm src/boot.py
 echo 'Transfering files:'
 echo '- PixelKit.py'
-ampy -p $1 put PixelKit.py
+ampy -p $1 put src/PixelKit.py
 echo '- PixelTurtle.py'
-ampy -p $1 put PixelTurtle.py
+ampy -p $1 put src/PixelTurtle.py
 echo '- microDNSSrv.py'
-ampy -p $1 put microDNSSrv.py
+ampy -p $1 put src/microDNSSrv.py
 echo '- microWebSrv.py'
-ampy -p $1 put microWebSrv.py
+ampy -p $1 put src/microWebSrv.py
 echo '- webrepl_cfg.py'
-ampy -p $1 put webrepl_cfg.py
+ampy -p $1 put src/webrepl_cfg.py
 echo '- boot.py'
-ampy -p $1 put boot.py
+ampy -p $1 put src/boot.py
 echo '- main.py'
-ampy -p $1 put main.py
+ampy -p $1 put src/main.py
 echo '- index.html'
 ampy -p $1 put ../www/dist/index.html.gz index.html
 echo '- docs.html'
